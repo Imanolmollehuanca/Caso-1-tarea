182f94fa676ee00fc9092f00993082e6a957fd14
Ángel Arias <ariasdelcarpioangelherber@gmail.com>
2026-09-14 22:21:17 -0500

    feat(informes-alertas): agregar modelos de alertas, usuarios y reportes

---
+++ Models/MovimientoHistorialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Caso_1_tarea.Models;
+
+public class MovimientoHistorialDto
+{
+    public int ProductoId { get; set; }
+    public string NombreProducto { get; set; } = null!;
+    public string TipoMovimiento { get; set; } = null!;
+    public int Cantidad { get; set; }
+    public DateTime Fecha { get; set; }
+}
---
+++ Models/RolUsuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Caso_1_tarea.Models;
+
+public enum RolUsuario
+{
+    Empleado,
+    Supervisor,
+    Administrador
+}
